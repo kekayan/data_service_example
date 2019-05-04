e7e476b92d6d189aa77663259976e20f7e33dc02
kekayan <kekayan@ieee.org>
2019-05-04 11:54:21 +0530

    fixes

--- app/main.py
+++ app/main.py
@@ -1,18 +1,26 @@
-from flask import Flask
+from flask import Flask,json,jsonify
 import requests
 
 app = Flask(__name__)
 
 @app.route("/<options>")
 def getdata(options):
+    url="https://gist.githubusercontent.com/mshafrir/2646763/raw/8b0dbb93521f5d6889502305335104218454c2bf/states_titlecase.json"
     if options=="codes":
         url="https://gist.githubusercontent.com/mshafrir/2646763/raw/8b0dbb93521f5d6889502305335104218454c2bf/states_hash.json"
-    elif options =="states":
+        response = requests.get(url)
+        data = response.text
+        return data
+    if options =="states":
         url="https://gist.githubusercontent.com/mshafrir/2646763/raw/8b0dbb93521f5d6889502305335104218454c2bf/states_titlecase.json"
-    response = requests.get(url)
-    data = response.text
-    return data
-
+        response = requests.get(url)
+        result = response.text
+        data={}
+        for record in json.loads(result):
+            data[record['name'].lower()]=record['abbreviation']
+            print(record)
+        return jsonify(data)
+    return "Error"
 @app.route("/")
 def welcome():
     return "welcome to dataservice"
